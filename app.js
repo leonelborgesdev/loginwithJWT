aab617872b2fe5632059dc9431b3d3cbd1485c2a
leonelborgesdev <leonel03t@gmail.com>
2024-04-17 11:15:33 -0400

    user login

--- app.js
+++ app.js
@@ -32,3 +32,25 @@ app.post("/register", async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+
+//User Login
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const isPasswordMatch = await bcrypt.compar(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({ message: " Invalid Credentials " });
+    }
+    const token = jwt.sing({ userId: user.id }, "your_secret_key_here", {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
